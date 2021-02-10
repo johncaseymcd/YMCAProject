19c7c008855c923d43d05c99b19dde43988f3837
Steve <slshotts77@gmail.com>
2021-02-09 20:30:13 -0500

    steve

---
+++ YMCAProject.Data/Invoice.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YMCAProject.Data
+{
+    public class Invoice
+    {
+        [Key]
+        public int InvoiceID { get; set; }
+                
+        [Required]
+        [ForeignKey(nameof(Members))]
+        public int MemberID { get; set; }
+        public virtual Member Members { get; set; }
+
+        [Required]
+        public DateTime InvoiceDate { get; set; }
+        public DateTime DueDate { get; set; }
+
+        //public virtual List<Course> Course { get; set; } = new List<Course>();
+
+        [Required]
+        public decimal Amount { get; set; }
+
+        //Public bool IsPaid
+    }
+}
---
+++ YMCAProject.Data/RequiredAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace YMCAProject.Data
+{
+    internal class RequiredAttribute : Attribute
+    {
+    }
+}
