d537e978c3c50cfad6ec63de312164b246d91bca
John Casey McDonough <johncaseymcd@gmail.com>
2021-02-18 18:35:39 -0500

    Created CourseCreate model. Fixing GitHub issues and re-adding branches. Now playing: Keepers Of Fellow Man by All That Remains

---
+++ YMCAProject.Models/CourseCreate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YMCAProject.Models
+{
+    public class CourseCreate
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public decimal Cost { get; set; }
+        [Required]
+        public int MaxCourseSize { get; set; }
+
+        // Stretch goal
+        // [Required]
+        // public int LocationID { get; set; }
+
+        // Stretch goal
+        // [Required]
+        // public int InstructorID { get; set; }
+    }
+}
