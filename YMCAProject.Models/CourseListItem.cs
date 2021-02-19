d61d8c6dac883deda1fa0aeba984ccd52a98de57
John Casey McDonough <johncaseymcd@gmail.com>
2021-02-18 19:17:49 -0500

    Added models for Course class. CourseCreate to add a new course, CourseEdit to update an existing course, CourseDetail for single-item queries, and CourseListItem for multi-item queries. Now playing: Myself by Lil B

---
+++ YMCAProject.Models/CourseListItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YMCAProject.Models
+{
+    public class CourseListItem
+    {
+        public int CourseID { get; set; }
+        public string CourseName { get; set; }
+        public decimal CourseCost { get; set; }
+        public int MaxCourseSize { get; set; }
+        public bool HasAvailability { get; set; }
+
+        // Stretch goal
+        // public string InstructorName { get; set; }
+
+        // Stretch goal 
+        // public string LocationName { get; set; }
+    }
+}
