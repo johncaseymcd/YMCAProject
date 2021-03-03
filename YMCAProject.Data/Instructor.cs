4eaddf4033b37e101ef0ab626c577e6e5f771b1a
John Casey McDonough <johncaseymcd@gmail.com>
2021-03-02 20:24:00 -0500

    Created Instructor class. Now playing: Crash Tactics by 65daysofstatic

--- YMCAProject.Data/Instructor.cs
+++ YMCAProject.Data/Instructor.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,37 @@ namespace YMCAProject.Data
 {
     public class Instructor
     {
+        [Key]
         public int InstructorID { get; set; }
+        [Required]
+        public string InstructorName { get; set; }
+        
+        [ForeignKey(nameof(Location))]
+        public int LocationID { get; set; }
+        public virtual Location Location { get; set; }
+
+        public ICollection<Course> CoursesTaught { get; set; } = new List<Course>();
+        public ICollection<Member> MembersTaught
+        {
+            get
+            {
+                var members = new List<Member>();
+                foreach(var course in CoursesTaught)
+                {
+                    foreach(var member in course.ListOfMembers)
+                    {
+                        members.Add(member);
+                    }
+                }
+                return members;
+            }
+
+            set { }
+        }
+
+        public Instructor()
+        {
+            CoursesTaught = new HashSet<Course>();
+        }
     }
 }
